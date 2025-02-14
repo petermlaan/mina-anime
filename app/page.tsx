ecb7bf4f1c6ffaabc10366389c24a9c90545f52d
Peter Laan <plaan70@gmail.com>
2025-02-14 19:14:42 +0100

    supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { createClient } from '@supabase/supabase-js'
 
-export default function Page() {
+export default async function Page() {
+
+  const supabase = createClient("https://xhffsmexnbsqdkybtppv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoZmZzbWV4bmJzcWRreWJ0cHB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1NDc5ODcsImV4cCI6MjA1NTEyMzk4N30.oAYrdTFTSW3gAMjjoD8HeMzioC-uE4cdHr7-zYDLgqY");
+
+  const { data, error } = await supabase
+    .from("user_anime_selections")
+    .select();
+
   return (
     <main>
       <form id="frmSearch">
@@ -12,7 +20,7 @@ export default function Page() {
           <input type="checkbox" id="chkShowList" />
         </label>
       </form>
-      <section id="main"></section>
+      <section id="main"><pre>{error + " - " + JSON.stringify(data)}</pre></section>
     </main>
   );
 }
