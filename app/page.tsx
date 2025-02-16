b9bc90d6b2499a824fba2689ca0d2ffc1c09ca9d
Peter Laan <plaan70@gmail.com>
2025-02-16 17:05:50 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 import React from "react";
-import { Anime } from '@tutkli/jikan-ts';
-import { createClient } from '@supabase/supabase-js'
-import { Cards } from "@/components/cards";
 import { Metadata } from "next";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import { SavedAnimes } from "@/components/savedanimes";
 
 export const metadata: Metadata = {
   title: "Mina Anime"
 };
 
 export default async function Page() {
-  const supabase = createClient(
-    process.env.SUPABASE_URL ?? "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY ?? "");
-
-  const { data, error } = await supabase
-    .from("user_anime_selections")
-    .select("anime_data");
-  if (error)
-    console.error(error);
-  let animes: Anime[] = [];
-  if (data)
-    animes = data[0].anime_data;
-
   return (
     <main className={styles.main}>
       <div className={styles.toprow}>
@@ -33,7 +18,7 @@ export default async function Page() {
           <input type="checkbox" id="chkShowList" />
         </label>
       </div>
-      <Cards animes={animes} />
+      <SavedAnimes showList={false} />
     </main>
   );
 }
