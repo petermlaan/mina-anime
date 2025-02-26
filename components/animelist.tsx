38768b010471cf519f0c87b9659c745fc77cce02
Peter Laan <plaan70@gmail.com>
2025-02-26 15:48:24 +0100

    more reliable sorting

--- components/animelist.tsx
+++ components/animelist.tsx
@@ -2,63 +2,67 @@
 
 import styles from "./animelist.module.css";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MyAnime } from "@/lib/interfaces";
 import { useAnimeContext } from "./animecontext";
 import MyRating from "./myrating";
 
 interface AnimeListProps {
     animes: MyAnime[];
-    search: boolean; // true for searchPage, false for Page.
+    search: boolean; // true for searchPage, false for Page (saved animes).
 }
-
 type TableColumn = "" | "title" | "score" | "myRating" | "watched";
 
-export function AnimeList({ animes, search }: AnimeListProps) {
+export function AnimeList({ animes, search = false }: AnimeListProps) {
     const ac = useAnimeContext();
     const [lastSort, setLastSort] = useState<TableColumn>("");
+    const [sortedAnimes, setSortedAnimes] = useState(animes);
+
+    useEffect(() => {
+        setSortedAnimes(animes);
+    }, [animes]);
 
     const onSort = (column: TableColumn) => {
         switch (column) {
             case "title":
                 if (lastSort === "title") {
-                    animes.sort((b, a) =>
-                        a.title_english.localeCompare(b.title_english));
+                    setSortedAnimes(sortedAnimes.sort((a, b) =>
+                        b.title_english.localeCompare(a.title_english)));
                     setLastSort("");
                 } else {
-                    animes.sort((b, a) =>
-                        b.title_english.localeCompare(a.title_english));
+                    setSortedAnimes(sortedAnimes.sort((a, b) =>
+                        a.title_english.localeCompare(b.title_english)));
                     setLastSort("title");
                 }
                 break;
-                case "score":
-                    if (lastSort === "score") {
-                        animes.sort((a, b) => a.score - b.score);
-                        setLastSort("");
-                    } else {
-                        animes.sort((a, b) => b.score - a.score);
-                        setLastSort("score");
-                    }
-                    break;
-                    case "myRating":
-                        if (lastSort === "myRating") {
-                            animes.sort((a, b) => a.myRating - b.myRating);
-                            setLastSort("");
-                        } else {
-                            animes.sort((a, b) => b.myRating - a.myRating);
-                            setLastSort("myRating");
-                        }
-                        break;
-                        case "watched":
-                            if (lastSort === "watched") {
-                                animes.sort(a => +a.watched);
-                                setLastSort("");
-                            } else {
-                                animes.sort(a => +!a.watched);
-                                setLastSort("watched");
-                            }
-                            break;
-                    }
+            case "score":
+                if (lastSort === "score") {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => a.score - b.score));
+                    setLastSort("");
+                } else {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => b.score - a.score));
+                    setLastSort("score");
+                }
+                break;
+            case "myRating":
+                if (lastSort === "myRating") {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => a.myRating - b.myRating));
+                    setLastSort("");
+                } else {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => b.myRating - a.myRating));
+                    setLastSort("myRating");
+                }
+                break;
+            case "watched":
+                if (lastSort === "watched") {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => +a.watched - +b.watched));
+                    setLastSort("");
+                } else {
+                    setSortedAnimes(sortedAnimes.sort((a, b) => +b.watched - +a.watched));
+                    setLastSort("watched");
+                }
+                break;
+        }
     };
 
     return (
@@ -67,10 +71,14 @@ export function AnimeList({ animes, search }: AnimeListProps) {
                 <thead>
                     <tr>
                         <th></th>
-                        {!search && <th><Link href={"#"} onClick={() => onSort("myRating")}>Sedd</Link></th>}
-                        <th><Link href={"#"} onClick={() => onSort("score")}>Poäng</Link></th>
-                        {!search && <th><Link href={"#"} onClick={() => onSort("myRating")}>Betyg</Link></th>}
-                        <th><Link href={"#"} onClick={() => onSort("title")}>Titel</Link></th>
+                        {!search && <th><Link href={"#"}
+                            onClick={() => onSort("watched")}>Sedd</Link></th>}
+                        <th><Link href={"#"}
+                            onClick={() => onSort("score")}>Poäng</Link></th>
+                        {!search && <th><Link href={"#"}
+                            onClick={() => onSort("myRating")}>Betyg</Link></th>}
+                        <th><Link href={"#"}
+                            onClick={() => onSort("title")}>Titel</Link></th>
                     </tr>
                 </thead>
                 <tbody>
