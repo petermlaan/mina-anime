66dd0a5e769bf334fb1d6a3cfeecb798a5dfcc32
Peter Laan <plaan70@gmail.com>
2025-03-05 21:16:55 +0100

    update

--- app/error.tsx
+++ app/error.tsx
@@ -6,20 +6,22 @@ export default function Error({
     error,
     reset,
 }: {
-    error: Error & { digest?: string };
+    error: Error & { digest?: string, response?: { status?: number } };
     reset: () => void;
 }) {
     const router = useRouter();
 
     let errormsg = error?.message ?? "Någonting gick fel.";
-    if (error?.name === "AxiosError")
+    if (error?.name === "AxiosError" && error?.response?.status === 404)
         errormsg = "Sidan kunde inte hittas.";
 
     return (
-        <main>
-            <h1>{errormsg}</h1>
-            <button onClick={() => reset()}>Försök igen</button>
-            <button onClick={() => router.push('/')}>Hem</button>
+        <main className="grid justify-center justify-items-center gap-4 mt-8">
+            <h1 className="text-2xl">{errormsg}</h1>
+            <div className="flex gap-4">
+                <button onClick={() => reset()}>Försök igen</button>
+                <button onClick={() => router.push('/')}>Hem</button>
+            </div>
         </main>
     );
 }
