59213d90c5c370e6387b410e9b27f2ebb970ff52
petermlaan <plaan70@gmail.com>
2025-06-05 19:37:27 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const DEBOUNCE_TEXT_DELAY = +(process.env.DEBOUNCE_TEXT_DELAY ?? 2000)
-export const DEBOUNCE_DB_DELAY = +(process.env.DEBOUNCE_DB_DELAY ?? 5000)
+export const DEBOUNCE_TEXT_DELAY = +(process.env.DEBOUNCE_TEXT_DELAY ?? 1000)
+export const DEBOUNCE_DB_DELAY = +(process.env.DEBOUNCE_DB_DELAY ?? 2000)
