780531a60a527e261cbbe289422ace34c0ad125d
petermlaan <plaan70@gmail.com>
2025-06-05 20:41:00 +0200

    Update linknp.tsx

--- components/linknp.tsx
+++ components/linknp.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function LinkNP({ children, href }: Readonly<{ children: React.ReactNode; href: string }>) {
     return (
-        <Link prefetch={false} href={href}>
+        <Link prefetch={false} href={href} rel={"noreferrer"}>
             {children}
         </Link>
     )
